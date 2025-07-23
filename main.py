ccbd583eabc810e7ec75dfd0fd8c4d7cc27ebbaa
MAjavanta <mahma@LAPTOP-5DR8I9FI.>
2025-07-23 23:30:40 +0100

    Set up initial request from API

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
+import requests
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+uri = "https://api.football-data.org/v4/matches"
+headers = {"X-Auth-Token": os.getenv("FOOTBALL_DATA_API_KEY")}
+
+
+def get_matches():
+    response = requests.get(uri, headers=headers)
+    print(response.json())
+
+
 def main():
-    print("Hello from football-dashboard!")
+    get_matches()
 
 
 if __name__ == "__main__":
